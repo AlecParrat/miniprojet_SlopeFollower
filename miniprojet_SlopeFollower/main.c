47787af70bedffda460740fb0b5dee2d38d3c69e
Thibault Schaller <thibault.schaller@epfl.ch>
2022-04-16 22:18:06 +0200

    Optimisation du code (ne compile pas !)

--- miniprojet_SlopeFollower/main.c
+++ miniprojet_SlopeFollower/main.c
@@ -17,8 +17,9 @@
 #include <sensors/imu.h>
 #include <sensors/mpu9250.h>
 #include <i2c_bus.h>
+#include <angle.c>
 
-//inits the messagebus with a mutexe and a condvar in order to transfer data from the IMU
+//inits the messagebus with a mutexe and a conditionnal variable in order to transfer data
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
@@ -69,43 +70,15 @@ int main(void)
     //starts the IMU
     imu_start();
 
-    //inits the inter-process communication bus
-    messagebus_init(&bus, &bus_lock, &bus_condvar);
-    messagebus_topic_t *imu_topic = messagebus_find_topic_blocking(&bus, "/imu");
-    imu_msg_t imu_values;
+	//inits the inter-process communication bus
+	imu_msg_t imu_values;
 
     //calibrates the IMU
     calibrate_acc();		//the offset values are stored in the structure imu_msg_t defined in the imu.h file
 
     /* Infinite loop. */
     while (1) {
-    	//wait for new measures to be published
-    	messagebus_topic_wait(imu_topic, &imu_values, sizeof(imu_values));
-
-    	//computes the angle according to the defined convention (left : [-180°, 0°] ; right : [0°, +180°]
-    	int16_t angle = 0;
-    	angle=(360/6.28)*atan(((float)imu_values.acc_raw[1]-(float)imu_values.acc_offset[1])/((float)imu_values.acc_raw[0]-(float)imu_values.acc_offset[0]));
-
-    	//corrects the angle value according to the orientation of the accelerometer (see axis printed on the body)
-    	// dial 1
-    	if(imu_values.acc_raw[0]-imu_values.acc_offset[0]>0 && imu_values.acc_raw[1]-imu_values.acc_offset[1]>0){
-    		angle = -angle-+90;
-    	}
-
-    	// dial 2
-    	if(imu_values.acc_raw[0]-imu_values.acc_offset[0]<0 && imu_values.acc_raw[1]-imu_values.acc_offset[1]>0){
-    		angle = -angle+90;
-    	}
-
-    	// dial 3
-    	if(imu_values.acc_raw[0]-imu_values.acc_offset[0]<0 && imu_values.acc_raw[1]-imu_values.acc_offset[1]<0){
-    		angle = -angle+90;
-    	}
-
-    	// dial 4
-    	if(imu_values.acc_raw[0]-imu_values.acc_offset[0]>0 && imu_values.acc_raw[1]-imu_values.acc_offset[1]<0){
-    		angle = -angle-90;
-    	}
+    	int16_t angle=compute_angle(imu_values, imu_topic);
 
     	//prints the computed angle value in RealTerm
     	chprintf((BaseSequentialStream *)&SD3, "%Angle_x=%-7d\r\n", angle);
