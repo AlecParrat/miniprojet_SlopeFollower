fdaa992df96b7ded2f1638a57b9a1bab7a9cfad2
Thibault Schaller <thibault.schaller@epfl.ch>
2022-04-28 11:08:52 +0200

    Gestion des alertes de proximité

--- miniprojet_SlopeFollower/prox.c
+++ miniprojet_SlopeFollower/prox.c
@@ -5,6 +5,17 @@
  *      Author: alecp
  */
 
+// Sensor numbers definition
+#define RIGHT_3 2		// IR3 on the body
+#define RIGHT_2 1		// IR2 on the body
+#define RIGHT_1 0		// IR1 on the body
+#define LEFT_1 7		// IR8 on the body
+#define LEFT_2 6		// IR7 on the body
+#define LEFT_3 5		// IR6 on the body
+
+// Proximity treshold
+#define PROXIMITY_TRESHOLD 1500
+
 #define GET_PROXIMITY_PERIOD 100
 
 #include <main.h>
@@ -12,6 +23,7 @@
 #include <msgbus/messagebus.h>
 #include <sensors/proximity.h>
 #include <chprintf.h>
+#include <stdbool.h>
 
 int get_proximity(int sensor_number) {
 	//sans calibration
@@ -30,20 +42,69 @@ static THD_FUNCTION(get_proximity_thd, arg){
 
 	systime_t time;
 
-	int16_t proxy_1 = 0;
-	int16_t proxy_2 = 0;
-	int16_t proxy_7 = 0;
-	int16_t proxy_8 = 0;
+	// Proximity variables
+	int16_t proxy_right_3 = 0;
+	int16_t proxy_right_2 = 0;
+	int16_t proxy_right_1 = 0;
+	int16_t proxy_left_1 = 0;
+	int16_t proxy_left_2 = 0;
+	int16_t proxy_left_3 = 0;
+
+	// Proximity alert
+	int8_t proximity_alert = 0;
+
+	// Alert values :
+
+	// 0 : no alert
+	// 1 : right_1 alert
+	// 2 : right_2 alert
+	// 3 : right_3 alert
+	// 4 : left_1 alert
+	// 5 : left_2 alert
+	// 6 : left_3 alert
 
 	while(1){
 
-		proxy_1=get_proximity(0);
-		proxy_2=get_proximity(1);
-		proxy_7=get_proximity(6);
-		proxy_8=get_proximity(7);
+		proxy_right_3 = get_proximity(RIGHT_3);
+		proxy_right_2 = get_proximity(RIGHT_2);
+		proxy_right_1 = get_proximity(RIGHT_1);
+		proxy_left_1 = get_proximity(LEFT_1);
+		proxy_left_2 = get_proximity(LEFT_2);
+		proxy_left_3 = get_proximity(LEFT_3);
 
-		chprintf((BaseSequentialStream *)&SD3, "Droite = %4d   Devant droite = %4d   Devant gauche = %4d   Gauche = %4d\r\n"
-		    									, proxy_2, proxy_1, proxy_8, proxy_7);
+		// alert on the right_3 :
+		if (proxy_right_3 > PROXIMITY_TRESHOLD && proxy_right_3 > proxy_right_2){
+			proximity_alert = 1;
+		}
+		// alert on the right_2 :
+		else if (proxy_right_2 > PROXIMITY_TRESHOLD && proxy_right_2 > proxy_right_1 && proxy_right_2 > proxy_left_1 && proxy_right_2 > proxy_left_2){
+			proximity_alert = 2;
+		}
+		// alert on the right_1 :
+		else if (proxy_right_1 > PROXIMITY_TRESHOLD && proxy_right_1 > proxy_right_2 && proxy_right_1 > proxy_left_1 && proxy_right_1 > proxy_left_2){
+			proximity_alert = 3;
+		}
+		// alert on the left_1 :
+		else if (proxy_left_1 > PROXIMITY_TRESHOLD && proxy_left_1 > proxy_right_2 && proxy_left_1 > proxy_right_1 && proxy_left_1 > proxy_left_2){
+			proximity_alert = 4;
+		}
+		// alert on the left_2 :
+		else if (proxy_left_2 > PROXIMITY_TRESHOLD && proxy_left_2 > proxy_right_2 && proxy_left_2 > proxy_right_1 && proxy_left_2 > proxy_left_1){
+			proximity_alert = 5;
+		}
+		// alert on theleft_3 :
+		else if(proxy_left_3 > PROXIMITY_TRESHOLD && proxy_left_3 > proxy_left_2){
+			proximity_alert = 6;
+		}
+		else{
+			proximity_alert = 0;
+		}
+
+		chprintf((BaseSequentialStream *)&SD3, "Alerte : %d\r\n", proximity_alert);
+
+
+//		chprintf((BaseSequentialStream *)&SD3, "Droite = %4d   Devant droite = %4d   Devant gauche = %4d   Gauche = %4d\r\n"
+//		    									, proxy_right, proxy_front_right, proxy_front_left, proxy_left);
 
 		chThdSleepUntilWindowed(time, time + MS2ST(GET_PROXIMITY_PERIOD));
 
