896141a6a03ba1627ac06cde9635a70ab17bee12
alec_parrat <alec.parrat@epfl.ch>
2022-05-15 09:49:59 +0200

    Ajout des fichiers de moyenne

---
+++ miniprojet_SlopeFollower/average.h
@@ -0,0 +1,13 @@
+/*
+ * average.h
+ *
+ *  Created on: 15 mai 2022
+ *      Author: alecp
+ */
+
+#ifndef AVERAGE_H_
+#define AVERAGE_H_
+
+int16_t average(int16_t new_value, int32_t* sum, int16_t* values, int16_t* counter, int16_t size);
+
+#endif /* AVERAGE_H_ */
